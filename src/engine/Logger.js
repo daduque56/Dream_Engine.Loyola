ca05ff7beb4902f75bcd68d2fd9ebf7950442844
David H. Duque <160485015+daduque56@users.noreply.github.com>
2024-03-04 09:45:59 +0100

    Add files via upload

---
+++ src/engine/Logger.js
@@ -0,0 +1,5 @@
+export default class Logger {
+    log(level, message, style){
+        const output = `%c[${level}] [${new Date().toISOString().replace(`T`,'.').split('.')[0]}] 
+    }
+}
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
+
