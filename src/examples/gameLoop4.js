572f598fd0a91ea8d1f3d06466a218a260722019
David H. Duque <160485015+daduque56@users.noreply.github.com>
2024-03-06 16:32:27 +0100

    Update gameLoop4.js
    // Uncaught SyntaxError: Identifier 'Dream_Engine' has already been declared (at gameLoop4.js:3:7)

--- src/examples/gameLoop4.js
+++ src/examples/gameLoop4.js
@@ -1,5 +1,5 @@
 import Dream_Engine from "../dream_Engine/Dream_Engine"
-
+// Uncaught SyntaxError: Identifier 'Dream_Engine' has already been declared (at gameLoop4.js:3:7)
 const stan = new Dream_Engine()
 
 stan.update = (dt) => {
