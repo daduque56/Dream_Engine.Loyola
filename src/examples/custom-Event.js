22c1a4bfd0520c13d7d58f0ea4de97f550344594
David H. Duque <160485015+daduque56@users.noreply.github.com>
2024-03-06 15:17:17 +0100

    Update custom-Event.js
    // Uncaught SyntaxError> Identifier 'Dream_Engine' has already been declared  at custom-Event.js:19:7

--- src/examples/custom-Event.js
+++ src/examples/custom-Event.js
@@ -1,5 +1,4 @@
 import Dream_Engine from "../dream_Engine/Dream_Engine";
-import Dream_Engine from "../dream_Engine/Dream_Engine";
 
 const collisionEvent = new  Event ("colissionEvent")
 window.addEventListener("collisonEvent", collisionHandler)
@@ -8,7 +7,7 @@ function collisionHandler (colissionEvent) {
     player.postion.x = 0
 }
 
-const worldWitdh = 100 
+const worldWidth = 100 
 const player = {
     speed: 10,
     postion: {
@@ -16,14 +15,14 @@ const player = {
         y: 0
     }
 }
-
+// Uncaught SyntaxError> Identifier 'Dream_Engine' has already been declared  at custom-Event.js:19:7
 const Dream_Engine = new Dream_Engine()
 
 Dream_Engine.update = (dt) => {
     Dream_Engine.logger.debug(player.postion.x)
     player.postion.x += player.speed * dt
 
-    if(player.postion.x > worldWitdh) {
+    if(player.postion.x > worldWidth) {
         window.dispatchEvent(collisionEvent)
     }
 }
