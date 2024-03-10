38b33268e4c590c8edeaaba7b061ad92efd89cd6
David H. Duque <160485015+daduque56@users.noreply.github.com>
2024-03-10 19:58:31 +0100

    (Updating Input.js)
    We´ve been having troubles attempting to connect and link VS code studio to our Repo. Now that we understood how to use Remote Explorer we are updating file by file before starting to use it

--- src/dream_Engine/Input.js
+++ src/dream_Engine/Input.js
@@ -1,4 +1,3 @@
-import Input from './Input'
 import Logger from './Logger'
 
 class Dream_Engine {
@@ -20,7 +19,30 @@ export default class Input {
         this.logger = Dream_Engine.logger
         this.logger.info('Input constructor called')
 
-        this.pressedKeys = {}
+        this.keys = {};
+        window.addEventListener('keydown', (event) => this.handleKeyDown(event));
+        window.addEventListener('keyup', (event) => this.handleKeyUp(event));
+
+        this.arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+    }
+
+    handleKeyDown(event) {
+        this.keys[event.key] = true;
+    }
+
+    handleKeyUp(event) {
+        this.keys[event.key] = false;
+    }
+
+    isKeyPressed(key) {
+        return this.keys[key] || false;
+    }
+
+    areArrowKeysPressed() {
+        return this.arrowKeys.some(key => this.isKeyPressed(key));
+    }
+ 
+      /*this.pressedKeys = {}
 
         window.addEventListener(
             "keydown",
@@ -29,7 +51,6 @@ export default class Input {
             }
         )
     }
-
     _setKeyValue(key, value)
     {
         this.pressedKeys [key] = true
@@ -37,6 +58,7 @@ export default class Input {
 
     isKeyPressed(key)
     {
-        return this.pressedKeys [keys]
-    }
+        return this.pressedKeys [keys]*/
+
 }
+
