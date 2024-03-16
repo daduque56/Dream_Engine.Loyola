e811628817736de61a9aaccd5676bcdc014428e3
David_Duque <dhduquebolanos@al.uloyola.es>
2024-03-16 18:23:22 +0100

    Inlcuded mouse inut logic and events and update console log print messages

--- src/examples/keyboard-controlled-movement.js
+++ src/examples/keyboard-controlled-movement.js
@@ -14,6 +14,40 @@ function mouseMoveHandler (event) {
     console.log(event)
 }
 
+let rightMouseDown = false;
+let leftMouseDown = false;
+
+window.addEventListener('contextmenu', (event) => {
+    event.preventDefault(); 
+});
+
+window.addEventListener('mouseup', (event) => {
+    if (event.button === 2) { 
+        rightMouseDown = false;
+        console.log("---->> PLayer stopped aiming <<----");
+    } else if (event.button === 0) { 
+        leftMouseDown = false;
+        console.log("---->> Player stopped shooting <<----");
+    }
+});
+
+window.addEventListener('mousedown', (event) => {
+    if (event.button === 2) { 
+        rightMouseDown = true;
+    } else if (event.button === 0) { 
+        leftMouseDown = true;
+    }
+});
+
+setInterval(() => {
+    if (rightMouseDown) {
+        console.log("---->> Player is aiming <<----");
+    }
+    if (leftMouseDown) {
+        console.log("---->> Player is shooting <<----");
+    }
+}, 100);
+
 const dream_Engine = new Dream_Engine()
 
 dream_Engine.update = (dt) => {
@@ -23,54 +57,39 @@ dream_Engine.update = (dt) => {
 
 dream_Engine.handleArrowKeyInput = (dt) => {
     if (dream_Engine.input.areArrowKeysPressed()) {
-        if (dream_Engine.input.isKeyPressed('ArrowRight')){
-            player.position.x  += player.speed * dt;
-            console.log("Player moved to the right")
-        }
-        if (dream_Engine.input.isKeyPressed('ArrowUp')){
-            player.position.y  += player.speed * dt;
-            console.log("Player moved up")
-        }
-        if (dream_Engine.input.isKeyPressed('ArrowLeft')){
-            player.position.x  -= player.speed * dt;
-            console.log("Player moved to the left")
-        }
-        if (dream_Engine.input.isKeyPressed('ArrowDown')){
-            player.position.y  -= player.speed * dt;
-            console.log("Player moved down")
         if (dream_Engine.input.isKeyPressed('KeyD')){
             player.position.x  += player.speed * dt;
-            console.log("Player moved to the right")
+            console.log("---->> Player moved to the right <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyW')){
-             player.position.y  += player.speed * dt;
-             console.log("Player moved up")
+            player.position.y  += player.speed * dt;
+            console.log("---->> Player moved up <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyA')){
-             player.position.x  -= player.speed * dt;
-             console.log("Player moved to the left")
+            player.position.x  -= player.speed * dt;
+            console.log("---->> Player moved to the left <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyS')){
             player.position.y  -= player.speed * dt;
-            console.log("Player moved down")
+            console.log("---->> Player moved down <<----")
         }
         if (dream_Engine.input.isKeyPressed('Space')){
-            console.log("Player jumped")
+            console.log("---->> Player jumped <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyR')){
-            console.log("Player is reloading")
+            console.log("---->> Player is reloading <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyE')){
-            console.log("Player is interacting")
+            console.log("---->> Player is interacting <<----")
         }
         if (dream_Engine.input.isKeyPressed('KeyF')){
-            console.log("Player is using an ability")
+            console.log("---->> Player is using an ability <<----")
         }
         if (dream_Engine.input.isKeyPressed('Escape')){
-            console.log("Player paused the game")
+            console.log("---->> Player paused the game <<----")
         }
     }
-    }   
 }
+
 dream_Engine.start()
     
