3aa545966b91bbd1c54dafda7fd9fb98588205ff
David_Duque <dhduquebolanos@al.uloyola.es>
2024-03-16 17:42:47 +0100

    Removing comment

--- src/examples/gameLoop4.js
+++ src/examples/gameLoop4.js
@@ -1,5 +1,5 @@
 import Dream_Engine from "../dream_Engine/Dream_Engine"
-// Uncaught SyntaxError: Identifier 'Dream_Engine' has already been declared (at gameLoop4.js:3:7)
+
 const Dream_Engine = new Dream_Engine()
 
 Dream_Engine.update = (dt) => {
