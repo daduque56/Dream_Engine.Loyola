106c869b9c4be837d1d64bbffd02c31dbded4bb8
David_Duque <dhduquebolanos@al.uloyola.es>
2024-03-18 09:16:15 +0100

    Creating our Mesh.js class

---
+++ src/dream_Engine/Mesh.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+
+export default class Mesh {
+    constructor(Dream_Engine) {
+        this.scene = Dream_Engine.scene;
+        this.logger = Dream_Engine.logger;
+        this.logger.info('Mesh constructor called');
+    }
+
+    CreateFromVertices (vertices, color) {
+        const geometry = new THREE.BufferGeometry();
+        const positionAttribute = new THREE.BufferAttribute(vertices, 3);
+        geometry.setAttribute('position', positionAttribute);
+        const material = new THREE.MeshBasicMaterial({ color: color });
+        const mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+    
+        return mesh;
+    }
+    CreateFromGeometry (geometry, color) {
+        const material = new THREE.MeshBasicMaterial({ color: color });
+        const mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+    
+        return mesh;
+    }
+}
+
