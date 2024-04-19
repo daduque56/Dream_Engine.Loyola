5f75fd2417cd1500a7d56bae7ca75862a31d2a3f
David_Duque <dhduquebolanos@al.uloyola.es>
2024-04-19 14:44:06 +0200

    Finishing details on shadows practice

--- src/examples/shadows.js
+++ src/examples/shadows.js
@@ -1,89 +1,45 @@
-import * as THREE from 'three'
-import Dream_Engine from '../dream_Engine/Dream_Engine.js'
+import * as THREE from 'three';
+import Dream_Engine from '../dream_Engine/Dream_Engine';
 
+const dream_Engine = new Dream_Engine();
+dream_Engine.camera.instance.position.set(200, 200, 200);
+dream_Engine.camera.instance.rotation.order = 'YXZ';
+dream_Engine.camera.instance.rotation.y = Math.PI / 4;
+dream_Engine.camera.instance.rotation.x = Math.atan(-1 / Math.sqrt(2));  
+dream_Engine.camera.instance.zoom = 20
+dream_Engine.camera.instance.updateProjectionMatrix()
 
-const dream_Engine = new Dream_Engine()
-dream_Engine.camera.instance.position.set(4, 3, 12)
+const ambientLight = dream_Engine.Light.CreateAmbientLight('white', 0.5)
 
-
-const ambientLight = dream_Engine.Light.CreateAmbientLight( "white", 0.5)
-
-
-const directionalLight = dream_Engine.Light.CreateDirectionalLight("yellow", 1)
-directionalLight.position.set(5, 1, 0)
-
-
+const directionalLight = dream_Engine.Light.CreateDirectionalLight('white', 1)
+directionalLight.position.set(5, 5, 1)
 directionalLight.castShadow = true
 
-
-
-
-
-
-
+const pointLight = dream_Engine.Light.CreatePointLight('purple', 50)  
+pointLight.position.set(0, 2, 0)
+pointLight.castShadow = true
 
 const planeMesh = dream_Engine.Mesh.CreateFromGeometry(
-
-
-    new THREE.PlaneGeometry(10,10),
-    new THREE.MeshStandardMaterial({color: 'white', wireframe: false})
-
-
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({color: 'black'})
 )
-
-
 planeMesh.rotateX(-Math.PI/2)
-planeMesh.position.x = 4
-
-
 planeMesh.receiveShadow = true
-       
-       
 
-
-const cubeMesh = dream_Engine.Mesh.CreateFromGeometry(
-
-
+const cubo = dream_Engine.Mesh.CreateFromGeometry(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshStandardMaterial( {color: 'orange', wireframe: false })
-
-
+    new THREE.MeshStandardMaterial({color: 'white'})
 )
-cubeMesh.position.set(5, 1, 0)
+cubo.position.set(0, 1, 0)
+cubo.castShadow = true
 
-
-cubeMesh.castShadow = true
-
-
-
-
-const capsuleMesh = dream_Engine.Mesh.CreateFromGeometry(
-    new THREE.CapsuleGeometry(1, 2, 2),
-    new THREE.MeshStandardMaterial({ color: 'grey', wireframe: false})
-)
-capsuleMesh.position.x = 5
-capsuleMesh.position.y = 3
-capsuleMesh.position.z = 1
-
-
-capsuleMesh.castShadow = false
-
+dream_Engine.scene.add(planeMesh, cubo, ambientLight, directionalLight, pointLight)
+dream_Engine.camera.instance.lookAt(cubo.position);
 
 dream_Engine.update = (dt) => {
-   
-
-
-    cubeMesh.rotateX(dt)
-    cubeMesh.rotateY(dt)
-
-
-    capsuleMesh.rotateX(dt)
-    capsuleMesh.rotateY(dt)
-
-
-
-
+    cubo.rotateX(dt)
+    cubo.rotateY(dt)
 }
 
+dream_Engine.start();
 
-dream_Engine.start()
