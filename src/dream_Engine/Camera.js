493d832a741bde650083a523e2aef04027824ddf
David_Duque <dhduquebolanos@al.uloyola.es>
2024-04-29 22:36:11 +0200

    Updating camera class to have a specific set every time is called

--- src/dream_Engine/Camera.js
+++ src/dream_Engine/Camera.js
@@ -8,19 +8,17 @@ export default class Camera
         this.logger.info("Camera constructor called")
         this.window = Dream_Engine.window
         this.scene = scene;
-
-        // PacMan Orthographic example camera
-        this.instance = new THREE.OrthographicCamera(
-            -this.window.width,
-            this.window.width,
-            this.window.height,
-            -this.window.height,
-        )
         
         // Isometric example camera
-        this.instance = new THREE.PerspectiveCamera(45, this.window.aspectRatio, 1, 1000)
+        this.instance = new THREE.PerspectiveCamera(
+            45,
+            this.window.aspectRatio,
+            1,
+            1000
+        )
+        this.instance.position.set(0, 0, 125)
         this.scene.add(this.instance)
-        this.instance.position.set(125, 125, 125);
+
     }
     setOrbitControls(canvas){
         this.controls = new THREE.OrbitControls(this.instance, canvas)
