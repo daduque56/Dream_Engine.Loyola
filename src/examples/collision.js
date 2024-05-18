4ba83b469a0c247af32ce2ff3ba90bca81a70b62
David_Duque <dhduquebolanos@al.uloyola.es>
2024-05-18 23:20:04 +0200

    Finisihing collision exercise

--- src/examples/collision.js
+++ src/examples/collision.js
@@ -1,64 +1,65 @@
-import * as THREE from 'three';
-import * as CANNON from 'cannon-es';
-import Dream_Engine from "../dream_Engine/Dream_Engine";
+import * as THREE from 'three'
+import * as CANNON from 'cannon-es'
+import Dream_Engine from "../dream_Engine/Dream_Engine"
 
-const collisionGame = new Dream_Engine();
-collisionGame.camera.instance.position.set(0, 10, 20);
-collisionGame.Physics.world.gravity.set(0, -9.81, 0);
+const game = new Dream_Engine()
+game.camera.instance.position.set(0, 10, 15)
+game.Physics.world.gravity.set(0, -9.81, 0)
 
-const ambientLight = collisionGame.Light.CreateAmbientLight('white', 0.5);
-const directionalLight = collisionGame.Light.CreateDirectionalLight('white', 0.7);
+const ambientLight = game.Light.CreateAmbientLight('white', 0.5)
+const directionalLight = game.Light.CreateDirectionalLight('white', 0.7)
 
-const ball = collisionGame.createObject();
-collisionGame.addComponentToObject(
+
+const ball = game.createObject()
+game.addComponentToObject(
     ball,
     'mesh',
-    collisionGame.Mesh.CreateFromGeometry(
+    game.Mesh.CreateFromGeometry(
         new THREE.SphereGeometry(1),
-        new THREE.MeshStandardMaterial({ color: 'crimson'})
+        new THREE.MeshStandardMaterial({ color: 'crimson' })
     )
 )
-collisionGame.addComponentToObject(
+game.addComponentToObject(
     ball,
     'rigidbody',
-    collisionGame.Physics.CreateBody({
+    game.Physics.CreateBody({
         mass: 1,
         shape: new CANNON.Sphere(1)
     })
 )
-ball.position.set(0, 20, 0);
+ball.castShadow = true
+ball.rigidbody.position.set(0, 5, 0)
 
-const floor = collisionGame.createObject();
-collisionGame.addComponentToObject(
-    floor,
+const gridHelper = game.Mesh.CreateGridHelper(10, 10, 'gray', 'white')
+gridHelper.position.y = 0.15
+
+const suelo = game.createObject()
+game.addComponentToObject(
+    suelo,
+    'mesh',
+    game.Mesh.CreateFromGeometry(
+        new THREE.BoxGeometry(10, 0.2, 10),
+        new THREE.MeshPhongMaterial({ color: 'darkslategray', transparent: true, opacity: 1  })
+    )
+)
+game.addComponentToObject(
+    suelo,
     'rigidbody',
-    collisionGame.Physics.CreateBody({
+    game.Physics.CreateBody({
         mass: 0,
-        shape: new CANNON.Plane()
+        shape: new CANNON.Box(new CANNON.Vec3(5, 0.1, 5))
     })
 )
-collisionGame.addComponentToObject(
-    floor,
-    'mesh',
-    collisionGame.Mesh.CreateFromGeometry(
-        new THREE.PlaneGeometry(15, 10),
-        new THREE.MeshPhongMaterial({ color: 'darkslategray' })
-    )
-)
-collisionGame.camera.instance.lookAt(0,0,0);
-floor.mesh.rotation.x = -Math.PI / 2;
+suelo.receiveShadow = true
 
-const plano = collisionGame.createObject();
-collisionGame.addComponentToObject(
-    plano,
-    'mesh',
-    collisionGame.Mesh.CreateFromGeometry(
-        new THREE.PlaneGeometry(15, 15),
-        new THREE.MeshPhongMaterial({ color: 'royalblue' })
-    )
-)
+console.log("ball Rb de update", ball.rigidbody.position);
+console.log("ball mesh de update", ball.mesh.position);
 
-plano.position.set(0, 0, 0);
-plano.mesh.rotateX(-Math.PI / 2);
 
-collisionGame.start();
+game.camera.instance.lookAt(ball.mesh.position)
+
+game.update = (dt) => {
+
+}
+
+game.start()
